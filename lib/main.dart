a5a0786f30d59903f40b82c01f21c855e26132af
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2023-08-28 12:54:48 +0300

    with some firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Data/Cubits/cubit/get_news_cubit.dart';
 import 'package:flutter_application_1/first.dart';
@@ -6,7 +7,13 @@ import 'package:flutter_application_1/second.dart';
 import 'package:flutter_application_1/third.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+import 'Api/Firebase_Api.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseApi().initNotification();
   runApp(const MyApp());
 }
 
